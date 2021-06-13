99ad451f632930730f08966e212a2be78060a6f3
Robert Marczak <robert.m.1305@gmail.com>
2021-06-13 23:50:47 +0100

    Create A Database
    Created the database with the 'National Parks' table

---
+++ ParkInAPI/Models/NationalPark.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkInAPI.Models
+{
+    public class NationalPark
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        public string Area { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime Established { get; set; }
+
+
+    }
+}
